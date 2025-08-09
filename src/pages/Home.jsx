f951a134a88a994c70af6a8c234ca3b12b44f87b
Anthony González <asgtoribio@gmail.com>
2025-08-09 04:40:06 +0000

    cargado las naves

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,25 @@
+import { useEffect, useState } from "react";
 import rigoImageUrl from "../assets/img/rigo-baby.jpg";
+import { CardNave } from "../components/CardNave.jsx";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
+
 
 export const Home = () => {
 
   const {store, dispatch} =useGlobalReducer()
+  const [naves,setNaves]= useState([])
+ 
 
+	useEffect(()=>{
+		console.log('cargado')
+		fetch('https://www.swapi.tech/api/starships')
+		.then((res)=> res.json())
+		.then((data)=>setNaves(data.results) )
+	},[])
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-		</div>
+		<>
+		<h1>Naves</h1>
+		{naves.map((nave)=> <CardNave key={nave.uid} name={nave.name} uid={nave.uid} /> )}
+		</>
 	);
 }; 
